2326d2e6c1532c4352dde0c8ce5ef40363a955c8
srengtrecom <srengtrecom@gmail.com>
2019-08-03 02:58:47 +0700

    edit android project.

--- android/app/src/main/java/com/inaday_app_5859/MainApplication.java
+++ android/app/src/main/java/com/inaday_app_5859/MainApplication.java
@@ -3,6 +3,7 @@ package com.inaday_app_5859;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.johnsonsu.rnsoundplayer.RNSoundPlayerPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import io.invertase.firebase.RNFirebasePackage;
@@ -32,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AsyncStoragePackage(),
             new RNSoundPlayerPackage(),
           new FBSDKPackage(),
           new RNFirebasePackage(),
