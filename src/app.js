7b7635d726d17351f116a3085b5a6259d4375223
srengtrecom <srengtrecom@gmail.com>
2019-08-10 13:13:05 +0700

    add pn module.

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import React from "react";
 import { mapping, light } from "@eva-design/eva";
 import { ApplicationProvider, Layout, Text } from "react-native-ui-kitten";
 import { Provider as ReduxProvider } from "react-redux";
+import { AsyncStorage } from 'react-native';
+import firebase from 'react-native-firebase';
 
 import { styles } from "./app.styles";
 import { store } from "./redux/store";
@@ -13,7 +15,39 @@ export default class App extends React.Component {
   };
 
   componentWillMount() {
+    this.checkPermission();
     this.loadAssets();
+  }
+
+  checkPermission = async () => {
+    const enabled = await firebase.messaging().hasPermission();
+    if (enabled) {
+      this.getToken();
+    } else {
+      this.requestPermission();
+    }
+  };
+
+  async getToken() {
+    let fcmToken = await AsyncStorage.getItem('fcmToken');
+    if (!fcmToken) {
+      fcmToken = await firebase.messaging().getToken();
+      if (fcmToken) {
+        // user has a device token
+        await AsyncStorage.setItem('fcmToken', fcmToken);
+      }
+    }
+  }
+
+  async requestPermission() {
+    try {
+      await firebase.messaging().requestPermission();
+      // User has authorised
+      this.getToken();
+    } catch (error) {
+      // User has rejected permissions
+      console.log('permission rejected');
+    }
   }
 
   loadAssets = async () => {
