089500f6866599a052aa27ef4c43e1908d22e7d6
srengtrecom <srengtrecom@gmail.com>
2019-08-15 00:23:07 +0700

    pre download.

--- src/containers/Signup/index.js
+++ src/containers/Signup/index.js
@@ -8,12 +8,15 @@ import {
   Alert,
   TouchableOpacity,
   Dimensions,
+  AsyncStorage
 } from 'react-native';
 import LoginInput from '../../components/LoginInput';
 import LoginButton from '../../components/LoginButton';
 import FBButton from '../../components/FBButton';
 import { signUp, signInWithFacebook } from '../../api/auth';
 import Loader from '../../components/Loader';
+import SoundPlayer from "react-native-sound-player";
+import firebase from "react-native-firebase";
 
 const { height } = Dimensions.get('screen');
 class Signup extends Component {
@@ -21,6 +24,39 @@ class Signup extends Component {
     email: '',
     password: '',
     loaderVisible: false,
+  };
+
+  async componentWillMount () {
+    await this.setInitPlayList();
+
+    SoundPlayer.addEventListener(
+      'FinishedLoadingURL',
+      async ({ success, url }) => {
+        if (success) {
+          console.log('download finished')
+          AsyncStorage.setItem(`${url}`, true)
+        }
+      });
+  }
+
+
+  setInitPlayList = async () => {
+    let firstPlayerRef = await firebase
+      .firestore()
+      .collection('files')
+      .orderBy('order', 'asc')
+      .limit(1);
+    await firstPlayerRef.get().then(async snapshot => {
+      let file = await snapshot.docs[0].data();
+      console.log(file);
+      let downloaded = AsyncStorage.getItem(`${file.url}`);
+      if (!downloaded) {
+        console.log('file is not downloaded');
+        await SoundPlayer.loadUrl(file.url);
+      } else  {
+        console.log('file is downloaded')
+      }
+    });
   };
 
   onChangeText = type => value => {
