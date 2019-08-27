06c9d36ff60c36927e91628f83699b262bc15230
Elijah Windsor <ewindsor@gmail.com>
2019-08-27 10:18:03 -0400

    Hiding splash screen once we know user's auth state, dispatching updateAuthUser

--- src/redux/sagas/auth.js
+++ src/redux/sagas/auth.js
@@ -1,6 +1,10 @@
-import { all, fork, spawn, call, cancelled, take } from 'redux-saga/effects'
+import { all, fork, spawn, call, cancelled, take, put } from 'redux-saga/effects'
 import { eventChannel, END, delay } from 'redux-saga'
 import firebase from 'react-native-firebase'
+import SplashScreen from 'react-native-splash-screen'
+import { updateAuthUser } from '../actions/auth'
+
+let splashScreenShowing = true
 
 export function* watchAuthRequests() {
   yield fork(watchUser)
@@ -14,10 +18,16 @@ function *watchUser() {
       const { user } = yield take(channel)
 
       if(!user) {
-
+        yield put(updateAuthUser(null))
       } else {
         const { uid } = user
 
+        yield put(updateAuthUser({ uid }))
+      }
+
+      if(splashScreenShowing === true) {
+        splashScreenShowing = false
+        SplashScreen.hide()
       }
     }
   } catch(e) {
